546f8f639da00bbe249b9b1227ad6d7e2b86c7d2
milan-itops <77695514+milan-itops@users.noreply.github.com>
2021-04-14 21:29:36 +0200

    Update install_wordpress.sh

--- wordpress-single-vm-ubuntu/install_wordpress.sh
+++ wordpress-single-vm-ubuntu/install_wordpress.sh
@@ -7,16 +7,12 @@ logger "Installing WordPress"
 # Set up a silent install of MySQL
 dbpass=$1
 
-export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
-
 # Install the LAMP stack and WordPress
-apt-get -y install apache2 mysql-server php7.4 php7.4-mysql wordpress
+apt-get -y install apache2 php7.4 php7.4-mysql wordpress
 
 # Setup WordPress
 gzip -d /usr/share/doc/wordpress/examples/setup-mysql.gz
-bash /usr/share/doc/wordpress/examples/setup-mysql -n wordpress localhost
+bash /usr/share/doc/wordpress/examples/setup-mysql -n wordpress -u itopsadm@demo-triple-mysql.mysql.database.azure.com -t demo-triple-mysql.mysql.database.azure.com -p $dbpass localhost
 
 ln -s /usr/share/wordpress /var/www/html/wordpress
 mv /etc/wordpress/config-localhost.php /etc/wordpress/config-default.php
